e529d56febcb08349165421ffada6a9ff9974f41
belvagen <belvagen@gmail.com>
2026-08-15 09:50:34 +0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,14 @@
-export default function handler(request, response) {
-    response.status(200).json({
+export default async function handler(request, response) {
+
+    if (request.method !== "POST") {
+        return response.status(405).json({
+            success: false,
+            message: "Используйте POST-запрос"
+        });
+    }
+
+    return response.status(200).json({
         success: true,
-        message: "QIAN получил запрос"
+        message: "QIAN получил изображение"
     });
 }
